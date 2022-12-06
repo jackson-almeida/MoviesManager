121f86bc81d957a1332bac51f0d187414e0c896c
Jackson de Almeida <jacksonalmeida@tokenlab.com.br>
2022-12-05 21:00:59 -0300

    Implementado controller

---
+++ app/src/main/java/com/example/moviesmanager/controller/MovieRoomController.kt
@@ -0,0 +1,59 @@
+package com.example.moviesmanager.controller
+
+import android.os.AsyncTask
+import androidx.room.Room
+import com.example.moviesmanager.model.dao.MovieRoomDao
+import com.example.moviesmanager.model.database.MovieRoomDaoDatabase
+import com.example.moviesmanager.view.MainActivity
+import com.example.moviesmanager.model.dao.MovieRoomDao.Constant.MOVIE_DATABASE_FILE
+import com.example.moviesmanager.model.entity.Movie
+
+class MovieRoomController(private val mainActivity: MainActivity) {
+    private val movieDaoImpl: MovieRoomDao by lazy {
+        Room.databaseBuilder(
+            mainActivity,
+            MovieRoomDaoDatabase::class.java,
+            MOVIE_DATABASE_FILE
+        ).build().getMovieRoomDao()
+    }
+
+    fun insertMovie(movie: Movie) {
+        Thread {
+            movieDaoImpl.createMovie(movie)
+            getMovies()
+        }.start()
+    }
+
+    fun getMovie(id: Int) = movieDaoImpl.retrieveMovie(id)
+
+    fun getMovies() {
+        object: AsyncTask<Unit, Unit, MutableList<Movie>>(){
+            override fun doInBackground(vararg params: Unit?): MutableList<Movie> {
+                val returnList = mutableListOf<Movie>()
+                returnList.addAll(movieDaoImpl.retrieveMovies())
+                return returnList
+            }
+
+            override fun onPostExecute(result: MutableList<Movie>?) {
+                super.onPostExecute(result)
+                if (result != null){
+                    mainActivity.updateMovieList(result)
+                }
+            }
+        }.execute()
+    }
+
+    fun editMovie(movie: Movie) {
+        Thread {
+            movieDaoImpl.updateMovie(movie)
+            getMovies()
+        }.start()
+    }
+
+    fun removeMovie(movie: Movie) {
+        Thread {
+            movieDaoImpl.deleteMovie(movie)
+            getMovies()
+        }.start()
+    }
+}
--- app/src/main/java/com/example/moviesmanager/view/MainActivity.kt
+++ app/src/main/java/com/example/moviesmanager/view/MainActivity.kt
@@ -3,10 +3,26 @@ package com.example.moviesmanager.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviesmanager.R
+import com.example.moviesmanager.adapter.MovieAdapter
+import com.example.moviesmanager.databinding.ActivityMainBinding
+import com.example.moviesmanager.model.entity.Movie
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var movieList: MutableList<Movie> = mutableListOf()
+    private lateinit var movieAdapter: MovieAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun updateMovieList(_movieList: MutableList<Movie>) {
+        movieList.clear()
+        movieList.addAll(_movieList)
+        movieAdapter.notifyDataSetChanged()
     }
 }
