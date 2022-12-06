a047c9d3f5abfb12b6733d940003c50735dc0770
Jackson de Almeida <jacksonalmeida@tokenlab.com.br>
2022-12-05 22:50:48 -0300

    Implementado activity de filme

--- app/src/main/java/com/example/moviesmanager/view/MovieActivity.kt
+++ app/src/main/java/com/example/moviesmanager/view/MovieActivity.kt
@@ -1,7 +1,10 @@
 package com.example.moviesmanager.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import java.util.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moviesmanager.databinding.ActivityMovieBinding
@@ -48,17 +51,57 @@ class MovieActivity : AppCompatActivity() {
                         amb.flagJaAssistidoCb.isEnabled = false
 
                         amb.generoSp.isEnabled = false
-                        amb.generoEt.isEnabled = false
-                        amb.salveBt.visibility = View.GONE
                         amb.generoSp.visibility = View.GONE
+
+                        amb.generoEt.isEnabled = false
                         amb.generoEt.visibility = View.VISIBLE
+
+                        amb.salveBt.visibility = View.GONE
                         amb.generoEt.setText(genero)
                     }
                 }
             }
         }
 
+        var generoSelecionado: String = ""
+        amb.generoSp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, indice: Int, id: Long) {
+                generoSelecionado = parent?.getItemAtPosition(indice).toString()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
 
+        amb.salveBt.setOnClickListener {
+            amb.salveBt.setOnClickListener {
+                // Verifica se o nome está vazio
+                if (amb.nomeEt.text.toString().length.equals(0)) Toast.makeText(this, "Nome inválido", Toast.LENGTH_LONG).show()
 
+                // Verifica se a flag foi ativada e se o range da nota dada está entre 0 e 10
+                if(amb.flagJaAssistidoCb.isChecked && (amb.notaEt.text.toString().toInt() < 0 || amb.notaEt.text.toString().toInt() > 10))
+                    Toast.makeText(this,"Essa nota não é válida!", Toast.LENGTH_LONG).show()
+
+                else {
+                    var campoNota: Int = 0
+                    if (!amb.flagJaAssistidoCb.isChecked ) campoNota = 0
+                    else amb.notaEt.text.toString().toInt()
+
+                    val movie = Movie(
+                        id = receivedMovie?.id ?: Random(System.currentTimeMillis()).nextInt(),
+                        nome = amb.nomeEt.text.toString(),
+                        anoLancamento = amb.lancamentoEt.text.toString().toInt(),
+                        estudio = amb.estudioProdutoraEt.text.toString(),
+                        duracao = amb.duracaoEt.text.toString().toInt(),
+                        flag = amb.flagJaAssistidoCb.isChecked,
+                        nota = campoNota,
+                        genero = generoSelecionado
+                    )
+
+                    val resultIntent = Intent()
+                    resultIntent.putExtra(EXTRA_MOVIE, movie)
+                    setResult(RESULT_OK, resultIntent)
+                    finish()
+                }
+            }
+        }
     }
 }
